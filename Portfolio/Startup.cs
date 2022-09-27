a4ef643182dd79b1efdf80fd8e3548697892fd92
HeisenbergHsueh <dhxue21@gmail.com>
2022-09-27 21:56:48 +0800

    1. 調整_Layout.cshtml的版面 2. 修改因為新增語系而造成的cookie authentication轉跳錯誤問題

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -22,6 +22,7 @@ using System.Globalization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Localization.Routing;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Portfolio
 {
@@ -63,17 +64,20 @@ namespace Portfolio
                 //browser 會限制 cookie 只能經由 http protocol 來存取
                 options.Cookie.HttpOnly = true;
                 //設定登入頁面，未登入時自動導入到登入頁面
-                options.LoginPath = new PathString("/LoginSystem/Login");
+                //options.LoginPath = new PathString("/LoginSystem/Login");
                 //設定登出頁面(此頁面可以省略)
-                options.LogoutPath = new PathString("/LoginSystem/Logout");
+                //options.LogoutPath = new PathString("/LoginSystem/Logout");
                 //當登入者不具有當下頁面的訪問權限時，會自動轉跳到此Forbidden頁面
-                options.AccessDeniedPath = new PathString("/LoginSystem/Forbidden");
+                //options.AccessDeniedPath = new PathString("/LoginSystem/Forbidden");
                 //設定登入有效時間(單位:分鐘)
                 options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
                 //SlidingExpiration = true，表示說，如果cookie當前的時間已經超過開始時間與結束時間的一半，則重新頒發cookie
                 //options.SlidingExpiration = true;
+                options.EventsType = typeof(HeisenbergHsuehCookieAuthenticationEvents);
             });
             #endregion
+
+            services.AddScoped<HeisenbergHsuehCookieAuthenticationEvents>();
 
             #region 授權
             services.AddAuthorization(options => {
@@ -112,6 +116,75 @@ namespace Portfolio
             });
             #endregion
         }
+
+        #region 動態改變cookie authentication的LoginPath、LogoutPath、AccessDenied
+        //因為網站設定了多國語系，因此導致user在進入需要登入or需要授權才看得見的頁面時，cookie authentication的跳轉頁面出現問題
+        //起因是由於，cookie authentication的LoginPath、LogoutPath、AccessDenied的路徑是寫死的，但因為語系設定，所以希望user在不同語系的頁面時
+        //可以跳轉到對應語系的頁面，因此以override的方式，重新改寫了RedirectToLogin、RedirectToLogout、RedirectToAccessDenied的跳轉方法
+        //參考資料(1) : https://social.msdn.microsoft.com/Forums/en-US/8626b4cf-be32-45e7-9704-e98c6d11b10f/how-can-i-set-dynamic-configureapplicationcookie?forum=aspdotnetcore
+        public class HeisenbergHsuehCookieAuthenticationEvents : CookieAuthenticationEvents
+        {
+            public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
+            {
+                string GetCurrentContextPath = context.Request.Path.ToString();
+
+                string SetCulture = "";
+
+                if(GetCurrentContextPath.Contains("zh-TW"))
+                {
+                    SetCulture = "zh-TW";
+                }
+                else if(GetCurrentContextPath.Contains("en-US"))
+                {
+                    SetCulture = "en-US";
+                }
+
+                context.RedirectUri = $"{context.Request.Scheme}://{context.Request.Host}/{SetCulture}/LoginSystem/Login";
+
+                return base.RedirectToLogin(context);
+            }
+
+            public override Task RedirectToLogout(RedirectContext<CookieAuthenticationOptions> context)
+            {
+                string GetCurrentContextPath = context.Request.Path.ToString();
+
+                string SetCulture = "";
+
+                if (GetCurrentContextPath.Contains("zh-TW"))
+                {
+                    SetCulture = "zh-TW";
+                }
+                else if (GetCurrentContextPath.Contains("en-US"))
+                {
+                    SetCulture = "en-US";
+                }
+
+                context.RedirectUri = $"{context.Request.Scheme}://{context.Request.Host}/{SetCulture}/LoginSystem/Logout";
+
+                return base.RedirectToLogout(context);
+            }
+
+            public override Task RedirectToAccessDenied(RedirectContext<CookieAuthenticationOptions> context)
+            {
+                string GetCurrentContextPath = context.Request.Path.ToString();
+
+                string SetCulture = "";
+
+                if (GetCurrentContextPath.Contains("zh-TW"))
+                {
+                    SetCulture = "zh-TW";
+                }
+                else if (GetCurrentContextPath.Contains("en-US"))
+                {
+                    SetCulture = "en-US";
+                }
+
+                context.RedirectUri = $"{context.Request.Scheme}://{context.Request.Host}/{SetCulture}/LoginSystem/Forbidden";
+
+                return base.RedirectToAccessDenied(context);
+            }
+        }
+        #endregion
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
